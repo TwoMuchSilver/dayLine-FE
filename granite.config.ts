73cf2f67063dd654679bcd5d26cdbc0788c80584
Dhani5703 <102973026+Dhani5703@users.noreply.github.com>
2025-11-24 15:21:48 +0900

    [WQA-15] feat: 기본 UI 페이지 및 구조 구현 (#1)
    * feat: 테스트환경 실행 위한 설정파일, 앱 아이콘, 의존성 설치 등 세팅
    
    * feat: 컴포넌트 라우팅 및 AIT 환경 설정
    
    * feat: IntroPage 컴포넌트 추가 및 사용자 안내 단계 구현
    
    * feat: 로그인 페이지 UI 컴포넌트 추가 및 하단 시트 동작 구현
    - 토스인증 로그인 로직 추후 수정 필요
    
    * feat: WritePage 컴포넌트 추가 및 입력 처리 기능 구현
    
    * feat: AdPage 임시 UI 구현
    
    * feat: StatsPage 컴포넌트 추가 및 완료/통계 화면 전환 기능 구현
    
    * feat: 바텀시트 컴포넌트 구현
    - 광고 동의, 로그인 동의 바텀시트
    
    * docs: PR 템플릿 추가

--- granite.config.ts
+++ granite.config.ts
@@ -5,14 +5,14 @@ export default defineConfig({
   brand: {
     displayName: '하루 한 줄', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
-    icon: '/assets/day_line_light.png', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
+    icon: 'src/assets/dayline_light_logo.png', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
     bridgeColorMode: 'basic',
   },
   web: {
-    host: 'localhost',
+    host: 'localhost', //실행하려면 개인 IP로로 변경 필요
     port: 5173,
     commands: {
-      dev: 'vite',
+      dev: 'vite --host',
       build: 'tsc -b && vite build',
     },
   },
---
+++ src/pages/IntroPage.tsx
@@ -0,0 +1,82 @@
+import {
+  Asset,
+  Post,
+  Paragraph,
+  StepperRow,
+  Stepper,
+  Button,
+} from '@toss/tds-mobile';
+import { adaptive } from '@toss/tds-colors';
+import { useNavigate } from 'react-router-dom';
+
+export default function Page() {
+  const navigate = useNavigate();
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '32px' }}>
+      <Post.H2 paddingBottom={24} color={adaptive.grey800} aria-label="앱 설명">
+      <div style={{ whiteSpace: 'nowrap' }}>
+        <Paragraph.Text>오늘 한 줄로 감정을 돌아보세요</Paragraph.Text>
+      </div>
+      </Post.H2>
+      <div>
+        <>
+          {/* 버튼으로 사용하는 경우 IconButton을 사용하거나 role="button"과
+          aria-label 값을 작성해주세요 */}
+          <Asset.Image
+            frameShape={Asset.frameShape.CleanW100}
+            backgroundColor="transparent"
+            src="https://static.toss.im/2d-emojis/png/4x/u1F4DD.png"
+            aria-hidden={true}
+            style={{ aspectRatio: '1/1' }}
+          />
+        </>
+      </div>
+      <Post.H2 paddingBottom={24} textAlign="left">
+        <Paragraph.Text>사용 방법</Paragraph.Text>
+      </Post.H2>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
+        <Stepper staggerDelay={0.5}>
+        <StepperRow
+          left={<StepperRow.NumberIcon number={1} />}
+          center={
+            <div style={{ whiteSpace: 'nowrap' }}>
+              <StepperRow.Texts
+                type="A"
+                title="한 문장으로 오늘 하루를 기록해요"
+                description=""
+              />
+            </div>
+          }
+        />
+        <StepperRow
+          left={<StepperRow.NumberIcon number={2} />}
+          center={
+            <div style={{ whiteSpace: 'nowrap' }}>
+              <StepperRow.Texts
+                type="A"
+                title="광고를 보는 동안 AI가 감정을 분석해요"
+                description=""
+              />
+            </div>
+          }
+          />
+        <StepperRow
+          left={<StepperRow.NumberIcon number={3} />}
+          center={
+            <div style={{ whiteSpace: 'nowrap' }}>
+              <StepperRow.Texts
+                type="A"
+                title="그래프로 감정 변화를 확인해요"
+                description=""
+              />
+            </div>
+          }
+          hideLine={false}
+        />
+        </Stepper>
+      </div>
+      
+      <Button display="block" onClick={() => navigate('/login')}>다음</Button>
+    </div>
+  );
+}
